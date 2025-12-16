c4692156b32990d6bf14a174a980005d5a4c18a5
PramudiWalakuluarachchi01 <pramudinethmini01@gmail.com>
2025-12-16 16:35:28 +0530

    Refactor code structure for improved readability and maintainability

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,6 +13,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Container(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Stack(
               children: [
@@ -22,11 +23,134 @@ class _HomeState extends State<Home> {
                   height: MediaQuery.of(context).size.height / 3,
                   fit: BoxFit.cover,
                 ),
-                 Padding(
-                   padding: const EdgeInsets.all(8.0),
-                   child: Image.asset("assets/images/boy.jpg",height: 50, width: 50,fit:BoxFit.cover ,),
-                 )
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 3,
+                  color: Colors.black.withOpacity(0.65),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, right: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Material(
+                        elevation: 3.0,
+                        borderRadius: BorderRadius.circular(60),
+
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(60),
+                          child: Image.asset(
+                            "assets/images/boy.jpg",
+                            height: 50,
+                            width: 50,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 110.0, left: 20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Travelers",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 80,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Caveat',
+                        ),
+                      ),
+                      const SizedBox(height: 1.0),
+
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50.0),
+                        child: Text(
+                          "Travel  Community App",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 209, 206, 206),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Karla',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 30.0,
+                    right: 30.0,
+                    top: MediaQuery.of(context).size.height / 3.3,
+                  ),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(30.0),
+
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(width: 1.5),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: "Search for places",
+                          prefixIcon: const Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 0.0,
+                            horizontal: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
+            ),
+            SizedBox(height: 40.0),
+            Container(
+              decoration: BoxDecoration(color: Colors.white),
+              margin: EdgeInsets.only(left: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(60),
+                        child: Image.asset(
+                          'assets/images/boy.jpg',
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        "Ann Perera",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Karla',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Image.asset('assets/images/img02.jpg'),
+                ],
+              ),
             ),
           ],
         ),
